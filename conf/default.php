a444017ff4febc408862d0101263125ad05f65e6
Aorimn <aorimn@gmail.com>
2013-08-16 19:20:50 +0200

    Change files location to the cachedir for them not to disclose private information

--- conf/default.php
+++ conf/default.php
@@ -5,9 +5,9 @@ $conf['tba_nb_attempt'] = 5;                  // If he failed 5 attempts to logi
 $conf['tba_mean_time']  = 120;                // Within 2 minutes
 $conf['tba_whitelist']  = '127.0.0.1';        // And is not on the white list
 
-$conf['tba_iptime_file'] = DOKU_PLUGIN.'tokenbucketauth/files/users.pbf'; // Log users attempts into this file
-$conf['tba_block_file']  = DOKU_PLUGIN.'tokenbucketauth/files/block.pbf'; // Log blocked users into this one
-$conf['tba_lockfile']    = DOKU_PLUGIN.'tokenbucketauth/files/lock.pbf';  // Lock file to know when we can put content into the two others
+$conf['tba_iptime_file'] = 'users.pbf';       // Log users attempts into this file
+$conf['tba_block_file']  = 'block.pbf';       // Log blocked users into this one
+$conf['tba_lockfile']    = 'lock.pbf';        // Lock file to know when we can put content into the two others
 
 $conf['tba_send_mail']   = '';                // Send email to admins when a user has been banned, leave blank if to noone
 
