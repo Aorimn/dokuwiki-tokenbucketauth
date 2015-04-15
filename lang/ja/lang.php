48860a297c5ddf0ec49e20104b15aad0b933250f
Hideaki SAWADA <chuno@live.jp>
2015-04-15 20:38:37 +0900

    Create lang.php

---
+++ lang/ja/lang.php
@@ -0,0 +1,19 @@
+<?php
+/**
+* @license GPL 2 (http://www.gnu.org/licenses/gpl.html)
+*
+* @author Hideaki SAWADA <chuno@live.jp>
+*/
+$lang['mailsubject'] = '[%s] 新規禁止 IP アドレス';
+ 
+$lang['ip']    = 'IP アドレス';
+$lang['host']  = 'ホスト名';
+$lang['date']  = '日付';
+$lang['del']   = '削除';
+$lang['noban'] = '現在、禁止 IP アドレスはありません。';
+ 
+$lang['page title']   = 'アクセス拒否';
+$lang['page content'] = 'この IP (%s) は %s で禁止されました。';
+ 
+$lang['del_success']    = 'IP (%s) は削除しました。';
+$lang['del_ipnotfound'] = 'IP (%s) はありません。';
